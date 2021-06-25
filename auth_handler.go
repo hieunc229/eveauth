420ea69faa7c1d26fc87e95f3f6c29e694ff6c57
Hieu C. Nguyen <nc.hieu@live.com>
2021-06-25 10:59:26 +0700

    [AuthHandler] fix handle error when err == nil

--- auth_handler.go
+++ auth_handler.go
@@ -6,10 +6,11 @@ import (
 
 func AuthHandler(nextFunc func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		if _, err := VerifyRequest(r); err == nil {
+		if _, err := VerifyRequest(r); err != nil {
 			handleError(w, err)
 			return
 		}
+
 		nextFunc(w, r)
 	}
 }
